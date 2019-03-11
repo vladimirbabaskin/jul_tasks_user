31dd7e51612046a5a508e0dab8942b13984b2ad6
Vasyl <admin@gmail.com>
2019-03-11 20:48:39 +0200

    created resourse routes.php for tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TaskController extends Controller
+{
+    public function index() {
+	echo 'all tasks';
+    }
+    public function create() {
+	
+    }
+    public function store() {
+	
+    }
+    public function show() {
+	
+    }
+    public function edit() {
+	
+    }
+    public function update() {
+	
+    }
+    public function destroy() {
+	
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('tasks', 'TaskController');
+
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
