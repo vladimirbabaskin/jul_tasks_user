8d3cacac289a78cf3fa92c7c482a3b7e0b56354e
Vasyl <admin@gmail.com>
2019-03-13 18:34:50 +0200

    auth middelwear

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,6 +8,9 @@ use App\Http\Requests;
 
 class TaskController extends Controller
 {
+    public function __construct() {
+	$this->middleware('auth');
+    }
     public function index() {
 	echo 'all tasks';
     }
